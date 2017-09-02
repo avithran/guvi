7a6de8902df3071581ef7675facf12051b678b72
bavithran durai <duraibavi14@gmail.com>
2017-09-02 14:22:01 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int number,remainder=0,r=0,original;
+original=number;
+scanf("%d",&number);
+while(number!=0)
+{
+remainder=number%10;
+r=r*10+remainder;
+number=number/10;
+}
+if(original==r)
+ printf("palindrome");
+else
+ printf("not paindrome");
+return 0;
+}
