d66cae8c9f7ce2d444c46c367eec7baa6569c78d
bavithran durai <duraibavi14@gmail.com>
2017-09-02 13:57:24 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -2,7 +2,7 @@ int main()
 {
 char input;
 scanf("%c",&input);
-if((input>'A'&&input<'Z')&&(input>'a'&&input<'z'))
+if((input>='A'&&input<='Z')&&(input>='a'&&input<='z'))
  printf("character is an alphabet");
 else
  printf("character is not an alphabet");
