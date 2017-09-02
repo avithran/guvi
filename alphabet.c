ecfee1500dfcc7ee0fa0d1b98db45b077a53a37d
bavithran durai <duraibavi14@gmail.com>
2017-09-02 11:29:34 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,10 @@
+int main()
+{
+char input;
+scanf("%c",&input);
+if((input>'A'&&input<'Z')&&(input>'a'&&input<'z'))
+ printf("character is an alphabet");
+else
+ printf("character is not an alphabet");
+return 0;
+}
