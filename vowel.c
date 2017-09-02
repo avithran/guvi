5269e41cc82919bb04d5be3e427d74dda9f61f18
bavithran durai <duraibavi14@gmail.com>
2017-09-02 11:16:17 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,10 @@
+int main()
+{
+char input;
+scanf("%c",&input);
+if(input='a'||input='e'||input='i'||input='o'||input='u'||input='A'||input='E'||input='I'||input='O'||input='U')
+ printf("vowel");
+else
+ printf("consonant");
+return 0;
+}
