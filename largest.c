4e929305e9d74eea8cf4ada22a80af57703d9205
bavithran durai <duraibavi14@gmail.com>
2017-09-02 11:41:38 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,12 @@
+int main()
+{
+int input1,input2,input3;
+scanf("%d,%d,%d",&input1,&input2,&input3);
+if(input1>input2 && input1>input3)
+ printf("%d is largest",%input1);
+elseif(input2>input3)
+ printf("%d is largest",%input2);
+else
+  printf("%d is largest",%input3);
+return 0;
+}
